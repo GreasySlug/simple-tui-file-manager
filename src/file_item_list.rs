fd2dce14b5970500edfd6e0bc5ef17a1f0be34d4
GreasySlug <9619abgoni@gmail.com>
2022-05-30 21:41:28 +0900

    add impl new methods

--- src/file_item_list.rs
+++ src/file_item_list.rs
@@ -16,8 +16,40 @@ struct FileItem {
     kinds: Kinds,
 }
 
+impl FileItem {
+    pub fn new(path: PathBuf, meta: Metadata) -> Self {
+        let kinds = determin_kinds_of_file_item(path.as_path());
+        let name = pathbuf_to_string_name(&path);
+        Self {
+            name,
+            path,
+            meta,
+            kinds,
+        }
+    }
+}
+
 #[derive(Debug, Clone)]
 struct Directory {
     name: String,
     path: PathBuf,
+}
+
+impl Directory {
+    pub fn new(path: PathBuf) -> Self {
+        let name = pathbuf_to_string_name(&path);
+        Self { name, path }
+    }
+}
+
+fn pathbuf_to_string_name(path: &PathBuf) -> String {
+    path.as_path().into_string()
+}
+
+fn determin_kinds_of_file_item(path: &Path) -> Kinds {
+    if path.is_dir() {
+        Kinds::Directory
+    } else {
+        Kinds::File
+    }
 }
