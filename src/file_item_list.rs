7001c72b198fe6be55296a2fca5e00072fca4899
GreasySlug <9619abgoni@gmail.com>
2022-06-08 22:24:10 +0900

    add: test for dir and kinds

--- src/file_item_list.rs
+++ src/file_item_list.rs
@@ -1,4 +1,4 @@
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
 use crate::path_process::pathbuf_to_string_name;
 
@@ -7,21 +7,55 @@ pub mod file_item;
 
 #[derive(Debug, Clone, PartialEq, PartialOrd)]
 pub enum Kinds {
-    File = 1,
-    Directory = 2,
+    File(bool),
+    Directory(bool),
 }
 
 impl Kinds {
     pub fn classifiy_kinds(path: &Path) -> Self {
         if path.is_dir() {
-            Self::Directory
+            Self::Directory(Self::is_hidden(path))
         } else {
-            Self::File
+            Self::File(Self::is_hidden(path))
         }
     }
 
     pub fn is_hidden(path: &Path) -> bool {
         let file_item_name = pathbuf_to_string_name(path);
         file_item_name.as_bytes()[0] == b'.'
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::path::Path;
+
+    use super::Kinds;
+
+    #[test]
+    fn is_this_hidden_files() {
+        let v = ["/dir/.dir/", "/.text.txt", ".config", ".../.../", "..."];
+
+        for name in v.into_iter() {
+            assert!(Kinds::is_hidden(Path::new(name)));
+        }
+    }
+
+    #[test]
+    fn classifiy_file_kinds_test() {
+        let v = [
+            ("/sample.rs", Kinds::File(false)),
+            ("sample.py", Kinds::File(false)),
+            (".sample", Kinds::File(true)),
+        ];
+        for (name, kinds) in v.into_iter() {
+            let path = Path::new(name);
+            assert_eq!(Kinds::classifiy_kinds(path), kinds);
+        }
+    }
+
+    #[test]
+    fn classifiy_dir_kinds_test() {
+        todo!();
     }
 }
