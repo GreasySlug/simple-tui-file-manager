c3aaea9562ce3d656155b272788cb9a74d3ee384
GreasySlug <9619abgoni@gmail.com>
2022-07-07 15:53:13 +0900

    add: draft help and version

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,49 @@ mod application;
 mod file_item_list;
 mod load_config;
 mod path_process;
+mod stacker;
 mod state;
 mod ui;
 
+const HELP_SENTENCE: &str = "#
+A Simple TUI File Manager => s(t)fm
+
+`sfm -V` | `sfm --version` => Display version
+`sfm -H` | `sfm --help` => Display help
+
+## Default Operation
+
+### Common Operation
+
+Normal | Input | Stacker
+h, j, k, l, Left, Up, Right, Down => cursor move
+tab, Shift+tab => move tab
+";
+
 fn main() -> Result<(), Box<dyn Error>> {
+    // 引数を受け取る
+    let args: Vec<String> = env::args().collect();
+    // sfm -V, sfm -H
+    let arg = &args.get(2);
+    match arg {
+        Some(arg) => match arg.as_str() {
+            "-V" | "--version" => {
+                let version: &str = env!("CARGO_PKG_VERSION");
+                println!("{version}");
+                return Ok(());
+            }
+            "-H" | "--help" => {
+                println!("{HELP_SENTENCE}");
+                return Ok(());
+            }
+            _ => {
+                println!("Invalid args");
+                return Ok(());
+            }
+        },
+        None => {}
+    }
+
     // setup terminal
     enable_raw_mode()?;
     let mut stdout = io::stdout();
@@ -30,12 +69,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     app.insert_new_statefuldir(crr_dir_path);
     app.push_new_dirname_to_dirtab(dir_name);
 
-    let home_dir_path = get_home_directory_path();
-    if let Some(path) = home_dir_path {
-        let dir_name = pathbuf_to_string_name(&path);
-        app.insert_new_statefuldir(path);
-        app.push_new_dirname_to_dirtab(dir_name);
-    }
+    // TODO: it is possible to config in ron file
+    // let home_dir_path = get_home_directory_path();
+    // if let Some(path) = home_dir_path {
+    //     let dir_name = pathbuf_to_string_name(&path);
+    //     app.insert_new_statefuldir(path);
+    //     app.push_new_dirname_to_dirtab(dir_name);
+    // }
 
     let res = run_app(&mut terminal, app);
 
